b318f0130c6da119b8a2443794e2d832cf0e6e22
Oriel12 <orielzvili@gmail.com>
2021-01-21 09:26:54 +0200

    This is my first exercise

---
+++ lesson 1/targil 2.py
@@ -0,0 +1,39 @@
+#####math:  *    |     +    |    -     |     **      |     /     |    //
+num=4567
+alafim=num//1000
+alafimm=alafim*1000
+meott=num-alafimm
+meot=meott//100
+asarott=meot*100
+asarottt=asarott+alafimm
+asarotttt=num-asarottt
+asarot=asarotttt//10
+ahadott=asarot*10
+ahadottt=alafimm+asarott+ahadott
+ahadotttt=num-ahadottt
+ahadot=ahadotttt//1
+
+
+
+
+
+print("Alafim= " + str(alafim))
+print("Meot= " + str(meot))
+print("Asarot= " + str(asarot))
+print("Ahadot= " + str(ahadot))
+
+
+
+
+
+
+
+
+
+'''
+matara
+alafim:4
+meot:5
+asarot=6
+ahadot=7
+'''
