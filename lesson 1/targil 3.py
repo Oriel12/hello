e373bdc3d5cd8aba5a08ab093bc216f64414c80e
Oriel12 <orielzvili@gmail.com>
2021-01-25 13:32:55 +0200

    This is my first exercise

---
+++ lesson 1/targil 3.py
@@ -0,0 +1,7 @@
+a="oriel zvili"
+b="oriel@gmail.com"
+c=13
+
+print("Full name: " + a + "\nEmail: " + b + "\nAge: " + str(c))
+print(a[-6::-1] + "\n " + str(c*3))
+print(a[0:5] in "idan ben dudu yuval shimon yael gal adam shahar yana")
